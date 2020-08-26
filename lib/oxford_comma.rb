22ee0f5c5fc5607058afa8b3aab98e8cd81db2b1
Trey Jennings <tjennings343@gmail.com>
2020-08-26 02:17:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
     array.join (" and ")
   else array.size >= 3
     long_array = array.pop
-    array.join(", #{, and}") << long_array
+    array.join(", ") << long_array
 end
 end
