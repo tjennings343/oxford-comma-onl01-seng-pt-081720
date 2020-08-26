7a530a0431fe9a803ded11fdfce5426c8a486053
Trey Jennings <tjennings343@gmail.com>
2020-08-26 02:39:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join (" ")
+  elsif array.size == 2
+    array.join (" and ")
+  else array.size >= 3
+    long_array = array.pop
+    array.push("and #{long_array}")
+    array.join(", ")
+end
 end
